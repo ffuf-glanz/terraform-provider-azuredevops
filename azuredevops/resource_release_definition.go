47bde4f1181d4ae810f45915ab5196051f06c52a
Kalarrs Topham <ktopham@opentechalliance.com>
2019-12-05 10:41:06 -0700

    Adding resource for release definition.

---
+++ azuredevops/resource_release_definition.go
@@ -0,0 +1,356 @@
+package azuredevops
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/config"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/converter"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/tfhelper"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/validate"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/build"
+)
+
+func resourceReleaseDefinition() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceReleaseDefinitionCreate,
+		Read:   resourceReleaseDefinitionRead,
+		Update: resourceReleaseDefinitionUpdate,
+		Delete: resourceReleaseDefinitionDelete,
+
+		Schema: map[string]*schema.Schema{
+			"revision": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "",
+			},
+			"path": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "\\",
+				ValidateFunc: validate.FilePathOrEmpty,
+			},
+
+			// TODO : Abstract this because it is used by build definitions and release definitions.
+			"variable_groups": {
+				Type: schema.TypeSet,
+				Elem: &schema.Schema{
+					Type:         schema.TypeInt,
+					ValidateFunc: validation.IntAtLeast(1),
+				},
+				MinItems: 1,
+				Optional: true,
+			},
+			"source": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice([]string{"ibiza", "portalExtensionApi", "restApi", "undefined", "userInterface"}, false),
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "",
+			},
+			// TODO : Abstract this because it is used by resource group and release definitions.
+			"variables": {
+				Type: schema.TypeSet,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"value": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Default:  "",
+						},
+						"is_secret": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Default:  false,
+						},
+					},
+				},
+				Required: true,
+				MinItems: 1,
+				Set: func(i interface{}) int {
+					item := i.(map[string]interface{})
+					return schema.HashString(item["name"].(string))
+				},
+			},
+			"releaseNameFormat": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "Release-$(rev:r)",
+			},
+
+			//"repository": {
+			//	Type:     schema.TypeSet,
+			//	Required: true,
+			//	MinItems: 1,
+			//	MaxItems: 1,
+			//	Elem: &schema.Resource{
+			//		Schema: map[string]*schema.Schema{
+			//			"yml_path": {
+			//				Type:     schema.TypeString,
+			//				Required: true,
+			//			},
+			//			"repo_name": {
+			//				Type:     schema.TypeString,
+			//				Required: true,
+			//			},
+			//			"repo_type": {
+			//				Type:         schema.TypeString,
+			//				Required:     true,
+			//				ValidateFunc: validation.StringInSlice([]string{"GitHub", "TfsGit"}, false),
+			//			},
+			//			"branch_name": {
+			//				Type:     schema.TypeString,
+			//				Optional: true,
+			//				Default:  "master",
+			//			},
+			//			"service_connection_id": {
+			//				Type:     schema.TypeString,
+			//				Optional: true,
+			//				Default:  "",
+			//			},
+			//		},
+			//	},
+			//},
+
+			"url": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"isDeleted": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceReleaseDefinitionCreate(d *schema.ResourceData, m interface{}) error {
+	clients := m.(*config.AggregatedClient)
+	buildDefinition, projectID, err := expandBuildDefinition(d)
+	if err != nil {
+		return fmt.Errorf("Error creating resource Build Definition: %+v", err)
+	}
+
+	createdBuildDefinition, err := createBuildDefinition(clients, buildDefinition, projectID)
+	if err != nil {
+		return fmt.Errorf("Error creating resource Build Definition: %+v", err)
+	}
+
+	flattenBuildDefinition(d, createdBuildDefinition, projectID)
+	return nil
+}
+
+func flattenBuildDefinition(d *schema.ResourceData, buildDefinition *build.BuildDefinition, projectID string) {
+	d.SetId(strconv.Itoa(*buildDefinition.Id))
+
+	d.Set("project_id", projectID)
+	d.Set("name", *buildDefinition.Name)
+	d.Set("repository", flattenRepository(buildDefinition))
+	d.Set("agent_pool_name", *buildDefinition.Queue.Pool.Name)
+
+	d.Set("variable_groups", flattenVariableGroups(buildDefinition))
+
+	revision := 0
+	if buildDefinition.Revision != nil {
+		revision = *buildDefinition.Revision
+	}
+
+	d.Set("revision", revision)
+}
+
+func flattenVariableGroups(buildDefinition *build.BuildDefinition) []int {
+	if buildDefinition.VariableGroups == nil {
+		return nil
+	}
+
+	variableGroups := make([]int, len(*buildDefinition.VariableGroups))
+
+	for i, variableGroup := range *buildDefinition.VariableGroups {
+		variableGroups[i] = *variableGroup.Id
+	}
+
+	return variableGroups
+}
+
+func createBuildDefinition(clients *config.AggregatedClient, buildDefinition *build.BuildDefinition, project string) (*build.BuildDefinition, error) {
+	createdBuild, err := clients.BuildClient.CreateDefinition(clients.Ctx, build.CreateDefinitionArgs{
+		Definition: buildDefinition,
+		Project:    &project,
+	})
+
+	return createdBuild, err
+}
+
+func resourceReleaseDefinitionRead(d *schema.ResourceData, m interface{}) error {
+	clients := m.(*config.AggregatedClient)
+	projectID, buildDefinitionID, err := tfhelper.ParseProjectIDAndResourceID(d)
+
+	if err != nil {
+		return err
+	}
+
+	buildDefinition, err := clients.BuildClient.GetDefinition(clients.Ctx, build.GetDefinitionArgs{
+		Project:      &projectID,
+		DefinitionId: &buildDefinitionID,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	flattenBuildDefinition(d, buildDefinition, projectID)
+	return nil
+}
+
+func resourceReleaseDefinitionDelete(d *schema.ResourceData, m interface{}) error {
+	if d.Id() == "" {
+		return nil
+	}
+
+	clients := m.(*config.AggregatedClient)
+	projectID, buildDefinitionID, err := tfhelper.ParseProjectIDAndResourceID(d)
+	if err != nil {
+		return err
+	}
+
+	err = clients.BuildClient.DeleteDefinition(m.(*config.AggregatedClient).Ctx, build.DeleteDefinitionArgs{
+		Project:      &projectID,
+		DefinitionId: &buildDefinitionID,
+	})
+
+	return err
+}
+
+func resourceReleaseDefinitionUpdate(d *schema.ResourceData, m interface{}) error {
+	clients := m.(*config.AggregatedClient)
+	buildDefinition, projectID, err := expandBuildDefinition(d)
+	if err != nil {
+		return err
+	}
+
+	updatedBuildDefinition, err := clients.BuildClient.UpdateDefinition(m.(*config.AggregatedClient).Ctx, build.UpdateDefinitionArgs{
+		Definition:   buildDefinition,
+		Project:      &projectID,
+		DefinitionId: buildDefinition.Id,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	flattenBuildDefinition(d, updatedBuildDefinition, projectID)
+	return nil
+}
+
+func flattenRepository(buildDefiniton *build.BuildDefinition) interface{} {
+	yamlFilePath := ""
+
+	// The process member can be of many types -- the only typing information
+	// available from the compiler is `interface{}` so we can probe for known
+	// implementations
+	if processMap, ok := buildDefiniton.Process.(map[string]interface{}); ok {
+		yamlFilePath = processMap["yamlFilename"].(string)
+	}
+
+	if yamlProcess, ok := buildDefiniton.Process.(*build.YamlProcess); ok {
+		yamlFilePath = *yamlProcess.YamlFilename
+	}
+
+	return []map[string]interface{}{{
+		"yml_path":              yamlFilePath,
+		"repo_name":             *buildDefiniton.Repository.Name,
+		"repo_type":             *buildDefiniton.Repository.Type,
+		"branch_name":           *buildDefiniton.Repository.DefaultBranch,
+		"service_connection_id": (*buildDefiniton.Repository.Properties)["connectedServiceId"],
+	}}
+}
+
+func expandBuildDefinition(d *schema.ResourceData) (*build.BuildDefinition, string, error) {
+	projectID := d.Get("project_id").(string)
+	repositories := d.Get("repository").(*schema.Set).List()
+
+	variableGroupsInterface := d.Get("variable_groups").(*schema.Set).List()
+	variableGroups := make([]build.VariableGroup, len(variableGroupsInterface))
+
+	for i, variableGroup := range variableGroupsInterface {
+		variableGroups[i] = *buildVariableGroup(variableGroup.(int))
+	}
+
+	// Note: If configured, this will be of length 1 based on the schema definition above.
+	if len(repositories) != 1 {
+		return nil, "", fmt.Errorf("Unexpectedly did not find repository metadata in the resource data")
+	}
+
+	repository := repositories[0].(map[string]interface{})
+
+	repoName := repository["repo_name"].(string)
+	repoType := repository["repo_type"].(string)
+	repoURL := ""
+	if strings.EqualFold(repoType, "github") {
+		repoURL = fmt.Sprintf("https://github.com/%s.git", repoName)
+	}
+
+	// Look for the ID. This may not exist if we are within the context of a "create" operation,
+	// so it is OK if it is missing.
+	buildDefinitionID, err := strconv.Atoi(d.Id())
+	var buildDefinitionReference *int
+	if err == nil {
+		buildDefinitionReference = &buildDefinitionID
+	} else {
+		buildDefinitionReference = nil
+	}
+
+	agentPoolName := d.Get("agent_pool_name").(string)
+	buildDefinition := build.BuildDefinition{
+		Id:       buildDefinitionReference,
+		Name:     converter.String(d.Get("name").(string)),
+		Path:     converter.String(d.Get("path").(string)),
+		Revision: converter.Int(d.Get("revision").(int)),
+		Repository: &build.BuildRepository{
+			Url:           &repoURL,
+			Id:            &repoName,
+			Name:          &repoName,
+			DefaultBranch: converter.String(repository["branch_name"].(string)),
+			Type:          &repoType,
+			Properties: &map[string]string{
+				"connectedServiceId": repository["service_connection_id"].(string),
+			},
+		},
+		Process: &build.YamlProcess{
+			YamlFilename: converter.String(repository["yml_path"].(string)),
+		},
+		Queue: &build.AgentPoolQueue{
+			Name: &agentPoolName,
+			Pool: &build.TaskAgentPoolReference{
+				Name: &agentPoolName,
+			},
+		},
+		QueueStatus:    &build.DefinitionQueueStatusValues.Enabled,
+		Type:           &build.DefinitionTypeValues.Build,
+		Quality:        &build.DefinitionQualityValues.Definition,
+		VariableGroups: &variableGroups,
+	}
+
+	return &buildDefinition, projectID, nil
+}
+
+func buildVariableGroup(id int) *build.VariableGroup {
+	return &build.VariableGroup{
+		Id: &id,
+	}
+}
